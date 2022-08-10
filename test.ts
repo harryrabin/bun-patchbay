084bb1bcaf34383b78758138fede98387c925b77
harryrabin <52754948+harryrabin@users.noreply.github.com>
2022-08-09 17:16:08 -0700

    tests

--- test.ts
+++ test.ts
@@ -19,31 +19,46 @@ class Test {
         } catch (e) {
             err = e;
         }
-        if (err) {
-            console.log(`Test "${this.name}" failed`);
-            console.log(`Error: ${err}\n`);
-        } else console.log(`Test "${this.name}" failed\n`);
+        if (err) console.log(`Test "${this.name}" failed\nError: ${err}\n`);
+        else console.log(`Test "${this.name}" failed\n`);
         process.exit(1);
     }
 }
 
 const tests: Test[] = [
-    new Test("patch route", () => {
-        const r = new PB.Route("/patchnamehere", "patch");
-        return r.re.toString() === "/^\\/patchnamehere\\/$/i";
-    }),
+    new Test("route creation", () => {
+        const r1 = new PB.Route("/patchnamehere", "patch");
+        if (r1.re.toString() !== "/^\\/patchnamehere\\/$/i")
+            throw "plain patch route was incorrect";
 
-    new Test("patch route w/ query string", () => {
-        const r = new PB.Route("/patchnamehere{queryString}", "patch");
-        return r.re.toString() === "/^\\/patchnamehere(?:\\?(.+))?\\/$/i";
-    }),
+        const r2 = new PB.Route("/patchnamehere{queryString}", "patch");
+        if (r2.re.toString() !== "/^\\/patchnamehere(?:\\?(.+))?\\/$/i")
+            throw "patch route w/ query string was incorrect";
 
-    new Test("router route", () => {
-        const r = new PB.Route("/routernamehere", "router");
-        return r.re.toString() === "/^\\/routernamehere(?=.*\\/$)/i";
+        const r3 = new PB.Route("/routernamehere", "router");
+        if (r3.re.toString() !== "/^\\/routernamehere(?=.*\\/$)/i")
+            throw "router route was incorrect";
+
+        return true;
     }),
 
     new Test("cookie handler", () => {
+        const req = new Request("");
+        req.headers.append("cookie", "cookieOne=one; cookieTwo=two");
+
+        const ch = new PB.CookieHandler();
+        ch.init(new PB.PBRequest(req));
+
+        if (ch.get("cookieOne") !== "one") throw "get() error";
+        if (ch.get("cookieTwo") !== "two") throw "get() error";
+
+        if (ch.stringify() !== undefined) throw "stringify() should have returned undefined";
+
+        ch.set("cookieThree", "three");
+
+        if (ch.stringify() !== '{"cookieOne":"one","cookieTwo":"two","cookieThree":"three"}')
+            throw "returned json from stringify() was incorrect"
+
         return true;
     })
 ]
