429dd3cbc458c46e3828cf077f1a583a4fbc071b
harryrabin <52754948+harryrabin@users.noreply.github.com>
2022-08-14 16:22:31 -0700

    Tests

--- test/test-bay.ts
+++ test/test-bay.ts
@@ -11,11 +11,28 @@ const AdminRouter = new PB.QuickRouter("/admin", [
     })
 ]);
 
+interface CatchallData {
+    text: string
+}
+class CatchallPatch extends PB.Patch<CatchallData> {
+    entry(req: Request) {
+        this.parseRouteParams();
+        return {
+            text: this.routeParameters.text || "null"
+        }
+    }
+
+    exit(data: CatchallData): Response {
+        return new Response(data.text);
+    }
+}
+
 const mainBay: PB.MainBay = {
     baseURL: "https://example.com",
     port: 443,
     patches: [
-        AdminRouter
+        AdminRouter,
+        new CatchallPatch("/{text}")
     ]
 }
 
